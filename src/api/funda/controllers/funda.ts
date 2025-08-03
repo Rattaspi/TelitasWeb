40f1984c7b37e21aebbb572ec5d262398e90e5aa
Alex Canut <a.canutperdigon@gmail.com>
2025-08-03 21:51:32 +0200

    Primer commit: proyecto Strapi inicial

---
+++ src/api/funda/controllers/funda.ts
@@ -0,0 +1,7 @@
+/**
+ * funda controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::funda.funda');
---
+++ src/api/funda/routes/funda.ts
@@ -0,0 +1,7 @@
+/**
+ * funda router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::funda.funda');
